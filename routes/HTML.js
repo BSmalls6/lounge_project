794e0cce4a80a46e090b402fba8612a63539090f
BSmalls6 <smallwoodbrennan@gmail.com>
2020-07-01 15:15:30 -0700

    changing routes for landing page

--- routes/HTML.js
+++ routes/HTML.js
@@ -2,38 +2,42 @@ var path = require("path");
 module.exports = function(app) {
 
     app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/html/landingpage.html"));
+    });
+
+    app.get("/home", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/main.html"));
-      });
-    
-      app.get("/kpop", function(req, res) {
+    });
+
+    app.get("/kpop", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/kpop.html"));
-      });
+    });
 
-      app.get("/rock", function(req, res) {
+    app.get("/rock", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/rock.html"));
-      });
+    });
 
-      app.get("/classical", function(req, res) {
+    app.get("/classical", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/classical.html"));
-      });
+    });
 
-      app.get("/jazz", function(req, res) {
+    app.get("/jazz", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/jazz.html"));
-      });
+    });
 
-      app.get("/edm", function(req, res) {
+    app.get("/edm", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/edm.html"));
-      });
+    });
 
-      app.get("/hiphop", function(req, res) {
+    app.get("/hiphop", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/views/html/hiphop.html"));
-      });
-    
-      // blog route loads blog.html
+    });
+
+    // blog route loads blog.html
     //   app.get("/blog", function(req, res) {
     //     res.sendFile(path.join(__dirname, "../public/blog.html"));
     //   });
-    
+
     //   // authors route loads author-manager.html
     //   app.get("/authors", function(req, res) {
     //     res.sendFile(path.join(__dirname, "../public/author-manager.html"));
@@ -41,5 +45,5 @@ module.exports = function(app) {
 
 
 
-    
+
 }
